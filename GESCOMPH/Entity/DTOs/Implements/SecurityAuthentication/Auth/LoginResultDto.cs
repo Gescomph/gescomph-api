c49478503b2949d59a6c67e5da51a557a7551c9b
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-11-08 23:29:18 -0500

    feat(HU-01-qa): project structuring

---
+++ GESCOMPH/Entity/DTOs/Implements/SecurityAuthentication/Auth/LoginResultDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Entity.DTOs.Implements.SecurityAuthentication.Auth
+{
+    public class LoginResultDto
+    {
+        public bool RequiresTwoFactor { get; set; }
+        public TokenResponseDto? Tokens { get; set; }
+        public TwoFactorChallengeDto? Challenge { get; set; }
+
+        public static LoginResultDto FromTokens(TokenResponseDto tokens) => new()
+        {
+            RequiresTwoFactor = false,
+            Tokens = tokens
+        };
+
+        public static LoginResultDto FromChallenge(TwoFactorChallengeDto challenge) => new()
+        {
+            RequiresTwoFactor = true,
+            Challenge = challenge
+        };
+    }
+}
