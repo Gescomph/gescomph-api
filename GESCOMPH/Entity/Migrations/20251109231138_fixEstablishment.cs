fe8ea3f89475c647118f18ca5c57b58052e85b4c
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-11-09 18:12:54 -0500

    fix(contract-pdf): corrects data initialization error (data init)

---
+++ GESCOMPH/Entity/Migrations/20251109231138_fixEstablishment.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixEstablishment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Active",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Active",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Active",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Active",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Active",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Active",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Active",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Active",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Active",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Establishments",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Active",
+                value: true);
+        }
+    }
+}
